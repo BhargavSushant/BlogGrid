c40348a3540f2787937646342712fcbe64f9184d
BhargavSushant <BhargavSushant@gmail.com>
2025-06-29 21:01:04 +0530

    fix(middlewares): correct middleware configuration by adding CORS settings

--- blog-cms-backend/config/middlewares.ts
+++ blog-cms-backend/config/middlewares.ts
@@ -1,4 +1,13 @@
 export default [
+    'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
