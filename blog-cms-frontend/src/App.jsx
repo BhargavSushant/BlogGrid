d056b11e9d4a19b500dda6a1805624b8d65f372f
BhargavSushant <BhargavSushant@gmail.com>
2025-06-29 20:41:46 +0530

    refactor(App): simplify component by removing unused state and logos, fetch posts from API

--- blog-cms-frontend/src/App.jsx
+++ blog-cms-frontend/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    fetch(`${apiUrl}/api/posts?populate=*`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Blog CMS Frontend</h1>
     </>
   )
 }
