ec0c3829579a5c5d9106ecdbb15e5c8ebdc1024a
BhargavSushant <BhargavSushant@gmail.com>
2025-06-30 11:16:27 +0530

    feat(App): enhance application layout with Vite and React logos, add interactive counter feat(favicon): add favicon to project assets

--- blog-cms-frontend/src/App.jsx
+++ blog-cms-frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 
 function App() {
+  const [count, setCount] = useState(0)
   
   useEffect(() => {
     fetch(`${apiUrl}/api/posts?populate=*`)
@@ -13,7 +16,26 @@ function App() {
 
   return (
     <>
-     <h1>Blog CMS Frontend</h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
